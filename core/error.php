006041a86a7a9c84b0b97186566e2877308a7a92
onedeko <vincent.chevalier@live.fr>
2015-01-30 12:15:08 +0100

    Commit-01
    Add core error.php for display error with uikit css

---
+++ core/error.php
@@ -0,0 +1,61 @@
+<?php namespace core;
+use core\controller,
+    core\view;
+
+class Error extends Controller {
+
+	/**
+	 * $error holder
+	 * @var string
+	 */
+	private $_error = null; 
+
+	/**
+	 * save error to $this->_error
+	 * @param string $error 
+	 */
+	public function __construct($error){
+		parent::__construct();
+		$this->_error = $error;
+	}
+
+	/**
+	 * load a 404 page with the error message
+	 */
+	public function index(){
+
+		header("HTTP/1.0 404 Not Found");
+		
+		$data['title'] = '404';
+		$data['error'] = $this->_error;
+		
+		View::rendertemplate('header',$data);
+		View::render('error/404',$data);
+		View::rendertemplate('footer',$data);
+		
+	}
+
+	/**
+	 * display errors
+	 * @param  array  $error an error of errors
+	 * @param  string $class name of class to apply to div
+	 * @return string        return the errors inside divs
+	 */
+	public static function display($error, $class = 'uk-alert uk-alert-danger'){
+		if (is_array($error)){
+
+			foreach($error as $error){
+				$row.= "<div class='$class'>$error</div>";
+			}
+			return $row;
+
+		} else {
+
+			if(isset($error)){
+				return "<div class='$class'>$error</div>";
+			}
+
+		}
+	}
+
+}
